df8abee1e82aeb93180282e907ea9de0fc0e22ef
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2025-01-23 16:10:39 +0300

    feat: add book model

---
+++ app/schemas/book.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+
+class Book(BaseModel):
+    id: int = Field(ge=0, description="Id of the book")
+    title: str = Field(
+        min_length=1, max_length=100, description="The title of the book"
+    )
+    author: str = Field(
+        min_length=1, max_length=100, description="The author of the book"
+    )
+    year: int = Field(
+        ge=1000, le=datetime.now().year, description="The year of publication"
+    )
+    genre: str = Field(min_length=1, max_length=50, description="The genre of the book")
