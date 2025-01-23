102fa7cf02f3c5d8fc500db53a24599f123daeda
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2025-01-23 20:18:40 +0300

    test: add integration test

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from worker.book_worker import BookWorker
 
 app = FastAPI()
 
-_book_worker = BookWorker()
+book_worker = BookWorker()
 
 
 # Эндпоинт для добавления новой книги
@@ -17,7 +17,7 @@ _book_worker = BookWorker()
     description="Create new book",
 )
 async def create_book(book: Book):
-    new_id, book = _book_worker.create_book(book)
+    new_id, book = book_worker.create_book(book)
     return {"id": new_id, "book": book}
 
 
@@ -27,7 +27,7 @@ async def get_books(
     genre: str | None = Query(None, description="Filter books by genre"),
     author: str | None = Query(None, description="Filter books by author"),
 ):
-    return _book_worker.get_all_books(genre, author)
+    return book_worker.get_all_books(genre, author)
 
 
 @app.get(
@@ -39,7 +39,7 @@ async def get_books(
     description="Get book by id",
 )
 async def get_book(book_id: int):
-    book = _book_worker.get_book(book_id)
+    book = book_worker.get_book(book_id)
     if book is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Book not found"
@@ -56,7 +56,7 @@ async def get_book(book_id: int):
     description="Update book by id",
 )
 async def update_book(book_id: int, updated_book: Book):
-    result = _book_worker.update_book(book_id=book_id, book_to_update=updated_book)
+    result = book_worker.update_book(book_id=book_id, book_to_update=updated_book)
     if result is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Book not found"
@@ -73,7 +73,7 @@ async def update_book(book_id: int, updated_book: Book):
     description="Delete book by id",
 )
 async def delete_book(book_id: int):
-    result = _book_worker.delete_book(book_id)
+    result = book_worker.delete_book(book_id)
     if result is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Book not found"
