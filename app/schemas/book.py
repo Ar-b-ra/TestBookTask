765979cccdedee2fa3eb9ad92463dc2ef2e561a0
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2025-01-23 17:03:36 +0300

    refactor: removed useless model

--- app/schemas/book.py
+++ app/schemas/book.py
@@ -3,20 +3,6 @@ from pydantic import BaseModel, Field
 
 
 class Book(BaseModel):
-    id: int
-    title: str = Field(
-        min_length=1, max_length=100, description="The title of the book"
-    )
-    author: str = Field(
-        min_length=1, max_length=100, description="The author of the book"
-    )
-    year: int = Field(
-        ge=1000, le=datetime.now().year, description="The year of publication"
-    )
-    genre: str = Field(min_length=1, max_length=50, description="The genre of the book")
-
-
-class BookCreate(BaseModel):
     title: str = Field(
         min_length=1, max_length=100, description="The title of the book"
     )
